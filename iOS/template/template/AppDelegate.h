5ff248c03e2f9df46762927f332a1deaeaa6f3ae
Cole <1255322700@qq.com>
2018-07-17 12:17:38 +0800

    init

---
+++ iOS/template/template/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  template
+//
+//  Created by Cole on 2018/7/17.
+//  Copyright © 2018年 Cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
